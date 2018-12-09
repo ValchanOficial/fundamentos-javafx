7909b3f6cb88935d8cddf3b3a882d3cc70cb0c97
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-12-09 16:11:55 -0200

    Utilizando SceneBuilder e ligando interface FXML com código Java

--- agenda.fx/src/main/java/br/com/treinaweb/agenda/fx/Main.java
+++ agenda.fx/src/main/java/br/com/treinaweb/agenda/fx/Main.java
@@ -3,14 +3,14 @@ package br.com.treinaweb.agenda.fx;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
